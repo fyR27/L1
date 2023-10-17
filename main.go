d78a01ad478c93f501572418f5b55a724b29eb36
Egor <130695351+fyR27@users.noreply.github.com>
2023-10-17 18:58:34 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-// маппим элементы поданые с массива строк
-func Mapping(x []string, c chan map[int]string) {
-	m := make(map[int]string)
-	for i := 0; i < len(x); i++ {
-		m[i+1] = x[i]
-	}
-	c <- m
-}
-
 func main() {
-	c := make(chan map[int]string)
-	x := []string{"first", "second", "third"}
-	go Mapping(x[:], c) // запускаем горутины чтобы на выходе получить размапленную строку
-	a := <-c
-	fmt.Println(a)
+	var x int64 = 10
+	var a string
+	var c string
+	b := 3
+	a = strconv.FormatInt(x, 2)
+	for i := 0; i < len(a); i++ {
+		if i == b {
+			if a[i] == uint8(48) { // проверяем если наша переменная равна 0 в уникоде тогда меняем ее на 1
+				c += "1"
+
+			} else { // в противном случае меняем на 0
+				c += "0"
+
+			}
+		} else if i != b { // если это не наша переенная просто записываем ее в новую строку
+			c += string(a[i])
+		}
+	}
+	fmt.Print(c)
 }
